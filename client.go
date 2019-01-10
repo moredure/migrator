16649d2aff1da2d12ea8b286c429282fde5807ff
mikefaraponov <mikefaraponov@gmail.com>
2019-01-10 20:36:19 +0200

    go:fmt

--- client.go
+++ client.go
@@ -2,7 +2,7 @@ package main
 
 type Client struct {
 	Flags string `client_list:"flags"`
-	Omem int64 `client_list:"omem"`
-	Obl int64 `client_list:"obl"`
-	Oll int64 `client_list:"oll"`
+	Omem  int64  `client_list:"omem"`
+	Obl   int64  `client_list:"obl"`
+	Oll   int64  `client_list:"oll"`
 }
--- client_list.go
+++ client_list.go
@@ -7,5 +7,3 @@ type ClientList []Client
 func (c *ClientList) UnmarshalBinary(data []byte) error {
 	return list.Unmarshal(data, c)
 }
-
-
